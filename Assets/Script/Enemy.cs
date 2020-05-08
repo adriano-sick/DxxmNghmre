351ec21deb6cd333f62d5c2f2cd873cac703205e
Adriano Siqueira <adriano_siqueira_silva@hotmail.com>
2020-05-08 10:42:45 -0300

    05-08

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float health = 50f;
+    private buttonControl_script enemyAnim;
+
+    private NavMeshAgent meshNav;
+    public GameObject player;
+    public float followDistance = 20.0f;
+    public float attackDistance = 10.0f;
+    [Range(0.0f, 1.0f)]
+    public float attackProbability = 50f;
+    [Range(0.0f, 1.0f)]
+    public float hitAccuracy = 0.5f;
+    public float damagePoints = 2.0f;
+    public AudioClip GunSound = null;
+    public AudioClip followSound = null;
+    private AudioSource enemySound;
+    public GameObject fire;
+    public Vector3 fireOffset = new Vector3(0, 3, 0);
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        meshNav = GetComponent<NavMeshAgent>();
+        enemySound = GetComponent<AudioSource>();
+        enemyAnim = GetComponent<buttonControl_script>();
+        
+        if (gameObject.name == "mummy@idle01")
+        {
+            enemyAnim.Idle();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyAnim.Idle();
+        
+
+        if (meshNav.enabled)
+        {
+            float dist = Vector3.Distance(player.transform.position, this.transform.position);
+            bool shoot = false;
+            bool follow = (dist < followDistance);
+            
+                           
+
+            if (follow)
+            {
+                float random = Random.Range(0.0f, 1.0f);
+                if (random > (1.0f - attackProbability) && dist < attackDistance)
+                {
+                    ShootEvent();
+                    GameObject temp = Instantiate(fire, gameObject.transform.position + fireOffset, transform.rotation) as GameObject;
+                    temp.transform.SetParent(transform);
+                }
+            }
+
+            if (follow)
+            {
+                meshNav.SetDestination(player.transform.position);
+                enemyAnim.Run();
+
+                //enemySound.PlayOneShot(followSound, 1f);
+            }
+
+            
+        }
+
+        
+    }
+
+    public void ShootEvent()
+    {
+        if (enemySound != null)
+        {
+            enemySound.PlayOneShot(GunSound);
+        }
+
+        float random = Random.Range(0.0f, 1.0f);
+
+        bool isHit = random > 1.0f - hitAccuracy;
+
+        PlayerMovement playerScript = GetComponent<PlayerMovement>();
+        
+
+        if (isHit)
+        {
+            playerScript.TakeDamage(damagePoints);
+        }
+    }
+        
+    public void TakeDamage (float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        if(gameObject.name == "mummy@idle01")
+        {
+            enemyAnim.Dance();
+        }
+        
+        Destroy(gameObject, 0.2f);
+    }
+}
---
+++ Assets/Script/Gun.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCam;
+
+    public ParticleSystem muzzleFlash;
+    public GameObject gunFlashLight;
+    public float gunFlashTime = 0.15f;
+    public GameObject impactEffect;
+    public float impactForce = 300f;
+
+    public float fireRate = 15f;
+    private float nextTimeToFire = 0f;
+
+    public AudioClip gunShoot;
+    private AudioSource gunSound;
+    public float SFXVol = 1f;
+
+   void Start()
+    {
+        gunSound = GetComponent<AudioSource>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1")  && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+        
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        StartCoroutine(Flashlight());
+        gunSound.PlayOneShot(gunShoot, SFXVol);
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Enemy enemy = hit.transform.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
+        }
+
+    }
+    IEnumerator Flashlight()
+    {
+        gunFlashLight.SetActive(true);
+        yield return new WaitForSeconds(gunFlashTime);
+        gunFlashLight.SetActive(false);
+    }
+
+
+
+}
